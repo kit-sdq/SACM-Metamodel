3b3644917b41b93f51180d5693e3e9f5ab2ba17b
Fabian Scheytt <fabian.scheytt@student.kit.edu>
2020-02-19 16:48:52 +0100

    Implemented Claim ID's using Sirius decorations
    further added asCited claim resources.

--- sacm.design/src/sacm/design/Services.java
+++ sacm.design/src/sacm/design/Services.java
@@ -1,17 +1,29 @@
 package sacm.design;
 
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.ecore.EObject;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
-    
-    /**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
-    }
+
+	/**
+	 * See
+	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
+	 * for documentation on how to write service methods.
+	 */
+	public EObject myService(EObject self, String arg) {
+		// TODO Auto-generated code
+		return self;
+	}
+
+	public IFigure renderTextLabel(EObject self, String text) {
+		Label l = new Label(text);
+		l.setBounds(new Rectangle(new Point(0, 0), l.getPreferredSize()));
+		return (IFigure) l;
+	}	
 }
