beb7d5fd3759db0d2988e09fd136f5dd1bd9642d
Fabian Scheytt <fabian.scheytt@student.kit.edu>
2020-03-09 16:46:33 +0100

    Implement axiomatic shape part

--- sacm.design.extensions/src/sacm/design/extensions/parts/shapes/AxiomaticRelationshipShape.java
+++ sacm.design.extensions/src/sacm/design/extensions/parts/shapes/AxiomaticRelationshipShape.java
@@ -9,14 +9,21 @@ public class AxiomaticRelationshipShape extends AbstractRelationshipShape {
 
 	@Override
 	public void paint(Graphics graphics, Rectangle area, Point targetDirection) {
-		paint(graphics, area);
+		int diameter = getShapeRadius(area);
+		double target_angle = calculateRelativeTargetAngle(area, targetDirection);
+		Point p1 = getPointAt(target_angle + Math.PI/2.0, diameter, area);
+		Point p2 = getPointAt(target_angle - Math.PI/2.0, diameter, area);
+		graphics.setLineWidth(4);
+		graphics.drawLine(p1, p2);
+		graphics.setLineWidth(2);
+		p1 = getPointAt(target_angle, diameter, area);
+		p2 = getPointAt(target_angle + Math.PI, diameter, area);
+		graphics.drawLine(p1, p2);
 	}
 
 	@Override
 	public void paint(Graphics graphics, Rectangle area) {
-		int diameter = (int)(Math.min(area.width, area.height)/1.5);
-		Point center = area.getCenter();
-		graphics.fillOval(center.x - diameter/2, center.y - diameter/2, diameter, diameter);
+		paint(graphics, area, area.getRight());
 	}
 
 	@Override
@@ -30,8 +37,13 @@ public class AxiomaticRelationshipShape extends AbstractRelationshipShape {
 	}
 	
 	private Point calculateConnectionPoint(Rectangle area, PrecisionPoint target, boolean isTargetAnchor) {
-		double radius = (Math.min(area.width, area.height)/1.5)/2.0;					
+		double radius = getShapeRadius(area); // Line Width					
 		double angle = Math.atan2(target.preciseY(), target.preciseX());
 		return (isTargetAnchor)?getPointAt(angle, radius, area):getPointAt(angle + Math.PI, radius, area);
+	}
+	
+	private int getShapeRadius(Rectangle area) {
+		// Minimum length - line width
+		return (Math.min(area.width, area.height) - 4)/2;
 	}
 }
