5ec1a9982a41e2740b61c5b9b4aa54510c01bf52
Fabian Scheytt <fabian.scheytt@student.kit.edu>
2020-03-14 19:58:07 +0100

    Add select dialogue for model instances from the workspace

--- sacm.design/src/sacm/design/Services.java
+++ sacm.design/src/sacm/design/Services.java
@@ -1,11 +1,23 @@
 package sacm.design;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.emf.common.ui.dialogs.ResourceDialog;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.diagram.impl.DNodeImpl;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.omg.sacm.argumentation.AssertedRelationship;
 
 /**
@@ -35,5 +47,33 @@ public class Services {
 		Label l = new Label(text);
 		l.setBounds(new Rectangle(new Point(0, 0), l.getPreferredSize()));
 		return (IFigure) l;
-	}	
+	}
+	
+	public List<EObject> selectAndQueryInstance(EObject self) {
+		final ResourceSetImpl resSet = new ResourceSetImpl();
+		//Select Library File
+		ResourceDialog fd = new ResourceDialog(Display.getDefault().getActiveShell(),
+				"Select a Resource containing the objects to import.", SWT.OPEN);
+		if (fd.open() == ResourceDialog.OK) {
+			for(URI uri: fd.getURIs()) {
+				try {
+					resSet.createResource(uri).load(null);
+				} catch (Exception e) {
+					System.err.println("Could not open selected URIs.");
+				}
+			}
+		}
+		Iterable<EObject> iterable = () -> EcoreUtil.getAllContents(resSet, true);
+		return StreamSupport.stream(iterable.spliterator(), false).collect(Collectors.toList());
+	}
+	
+	public List<EObject> preSortCitedElements(EObject self, List<EObject> list) {
+		// Sort selected elements with relationships being last
+		Collections.sort(list, (e1, e2) -> {
+			return 1;
+		});
+		return list;
+	}
+	
+	
 }
