dd1f0b90f3e9ada4c6dd38c4470e4139839e20e8
Fabian Scheytt <fabian.scheytt@student.kit.edu>
2020-03-23 11:50:17 +0100

    Fix Creation of ArgumentPackageBindings and modify representations

--- sacm.design/src/sacm/design/Services.java
+++ sacm.design/src/sacm/design/Services.java
@@ -1,8 +1,8 @@
 package sacm.design;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -11,32 +11,25 @@ import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.ui.dialogs.ResourceDialog;
-import org.eclipse.emf.common.util.BasicEList;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreEList;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.diagram.impl.DNodeImpl;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.omg.sacm.SacmFactory;
-import org.omg.sacm.SacmPackage;
 import org.omg.sacm.argumentation.ArgumentAsset;
 import org.omg.sacm.argumentation.ArgumentGroup;
 import org.omg.sacm.argumentation.ArgumentPackage;
 import org.omg.sacm.argumentation.ArgumentPackageBinding;
 import org.omg.sacm.argumentation.ArgumentReasoning;
 import org.omg.sacm.argumentation.ArgumentationFactory;
-import org.omg.sacm.argumentation.ArgumentationPackage;
 import org.omg.sacm.argumentation.AssertedRelationship;
 import org.omg.sacm.argumentation.Assertion;
 import org.omg.sacm.argumentation.Claim;
 import org.omg.sacm.package_.AssuranceCasePackage;
-import org.omg.sacm.package_.PackageFactory;
 
 /**
  * The services class used by VSM.
@@ -95,6 +88,9 @@ public class Services {
 					System.err.println("Could not open selected URIs.");
 				}
 			}
+		} else {
+			// Throw exception to cancel library selection
+			throw new IllegalArgumentException("Select library operation cancelled");
 		}
 		/* Filter results of EcoreUtil.getAllContents <b>at least for EObjects</b>
 		   as not all Contents must be EObjects 	*/
@@ -120,46 +116,46 @@ public class Services {
 		SACMElementCopier cp = new SACMElementCopier(true, false);
 		Collection<EObject> copyList = cp.copyAll(flatList);
 		cp.copyReferences();
-		insertCopiedElements(self, copyList);
+		Set<ArgumentPackage> packages = flatList.stream().filter(e -> e.eContainer() instanceof ArgumentPackage)
+				.map(e->(ArgumentPackage)e.eContainer()).collect(Collectors.toSet());
+		insertCopiedElements(self, copyList, packages);
 	}
 	
-	public void insertCopiedElements(EObject self, Collection<EObject> copies) {
-		if (self instanceof ArgumentPackage) {
-			List<ArgumentPackage> packages = new BasicEList<ArgumentPackage>();
+	public void insertCopiedElements(EObject self, Collection<EObject> copies, Set<ArgumentPackage> packages) {
+		if (self instanceof ArgumentPackage) {			
+			packages.add((ArgumentPackage)self);
 			for(EObject e : copies) {
 				if(e instanceof ArgumentAsset) {
 					((ArgumentPackage) self).getArgumentationElement().add((ArgumentAsset)e);
-					if (! packages.contains(e.eContainer()) && e.eContainer() instanceof ArgumentPackage)
-						packages.add((ArgumentPackage)e.eContainer());
 				}
 			}
 			// If the ArgumentPackage and imported Packages lie within a common Package
 			// create a common Package Binding if none exists
 			if (self.eContainer() instanceof AssuranceCasePackage) {
 				AssuranceCasePackage assurancePackage = (AssuranceCasePackage)self.eContainer();
-				if (assurancePackage.getArgumentPackage().stream().
-						filter(p -> p instanceof ArgumentPackageBinding 
-						&& ((ArgumentPackageBinding)p).getParticipantPackage().equals(packages)).count() == 0) {
+				List<ArgumentPackage> existingBindings = assurancePackage.getArgumentPackage().stream()
+						.filter(p -> p instanceof ArgumentPackageBinding)
+						.map(p -> (ArgumentPackageBinding)p)
+						.filter(p -> packages.containsAll(p.getArgumentationElement())).collect(Collectors.toList());
+				if (existingBindings.size() == 0) {
 					ArgumentPackageBinding binding = ArgumentationFactory.eINSTANCE.createArgumentPackageBinding();
 					binding.getParticipantPackage().addAll(packages);
-					binding.getParticipantPackage().add((ArgumentPackage)self);
 					binding.setName(SacmFactory.eINSTANCE.createLangString());
 					assurancePackage.getArgumentPackage().add(binding);
 				}				
 			} else if(self.eContainer() instanceof ArgumentPackage) {
 				ArgumentPackage argPackage = (ArgumentPackage)self.eContainer();
-				if (argPackage.getArgumentationElement().stream().
-						filter(p -> p instanceof ArgumentPackageBinding 
-						&& ((ArgumentPackageBinding)p).getParticipantPackage().equals(packages)).count() == 0) {
+				List<ArgumentPackage> existingBindings = argPackage.getArgumentationElement().stream()
+						.filter(p -> p instanceof ArgumentPackageBinding)
+						.map(p -> (ArgumentPackageBinding)p)
+						.filter(p -> packages.containsAll(p.getArgumentationElement())).collect(Collectors.toList());
+				if (existingBindings.size() == 0) {
 					ArgumentPackageBinding binding = ArgumentationFactory.eINSTANCE.createArgumentPackageBinding();
 					binding.getParticipantPackage().addAll(packages);
-					binding.getParticipantPackage().add((ArgumentPackage)self);
 					binding.setName(SacmFactory.eINSTANCE.createLangString());
 					argPackage.getArgumentationElement().add(binding);
 				}
 			}
 		}
-	}
-	
-	
+	}	
 }
